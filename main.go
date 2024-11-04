5dd26209178b1b11bf5bbf09184a7dae8e5e480c
Roberta Madge <srtabeta@gmail.com>
2024-11-04 16:56:52 -0300

    Cloud config

--- main.go
+++ main.go
@@ -2,20 +2,22 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/belmadge/temperature-cep/handlers"
 
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
 	}
 
 	router := gin.Default()
 	router.GET("/weather/:cep", handlers.WeatherHandler)
-	router.Run(":8080")
+
+	log.Printf("Starting server on port %s...", port)
+	router.Run(":" + port)
 }
